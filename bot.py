867cfdeb5152971c3945ca34f82559ca4196e481
Artem Zobkov <zobkovartyom@gmail.com>
2025-04-24 18:28:50 +0300

    добавил импорт модулей))))

---
+++ bot.py
@@ -0,0 +1,15 @@
+import logging
+import asyncio
+
+from config import bot_token
+
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import Router
+from aiogram import F
